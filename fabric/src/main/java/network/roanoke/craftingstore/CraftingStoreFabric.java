5c67eb837170e8854dcefc30d432efd0894255e4
Xander Forrest <xander@xanderforrest.com>
2023-09-12 16:45:57 +0100

    Implement executeDonation (without cancellable event)

--- fabric/src/main/java/network/roanoke/craftingstore/CraftingStoreFabric.java
+++ fabric/src/main/java/network/roanoke/craftingstore/CraftingStoreFabric.java
@@ -2,13 +2,24 @@ package network.roanoke.craftingstore;
 
 import net.craftingstore.core.CraftingStore;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
 
 public class CraftingStoreFabric implements ModInitializer {
 
     private CraftingStore craftingStore;
+    private MinecraftServer server;
 
     @Override
     public void onInitialize() {
         this.craftingStore = new CraftingStore(new CraftingStoreFabricImpl(this));
+
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            this.server = server;
+        });
+    }
+
+    public MinecraftServer getServer() {
+        return this.server;
     }
 }
--- fabric/src/main/java/network/roanoke/craftingstore/CraftingStoreFabricImpl.java
+++ fabric/src/main/java/network/roanoke/craftingstore/CraftingStoreFabricImpl.java
@@ -1,5 +1,6 @@
 package network.roanoke.craftingstore;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.craftingstore.core.CraftingStorePlugin;
 import net.craftingstore.core.PluginConfiguration;
 import net.craftingstore.core.logging.CraftingStoreLogger;
@@ -14,6 +15,21 @@ public class CraftingStoreFabricImpl implements CraftingStorePlugin {
     }
     @Override
     public boolean executeDonation(Donation donation) {
+        if (donation.getPlayer().isRequiredOnline()) {
+            if (fabricPlugin.getServer().getPlayerManager().getPlayerList().stream().noneMatch(player -> player.getUuid().equals(donation.getPlayer().getUUID()))) {
+                return false;
+            }
+        }
+
+        // implement event so it can be cancelled
+
+        try {
+            fabricPlugin.getServer().getCommandManager().getDispatcher().execute(donation.getCommand(), fabricPlugin.getServer().getCommandSource());
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+            return false;
+        }
+
         return false;
     }
 
